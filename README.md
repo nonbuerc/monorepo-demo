d423a527e231d493b397cb40d8dcc3b66d8bb69b
cheng <gnehcheng@yeah.net>
2022-08-15 16:04:11 +0800

    .

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # monorepo-demo
+
+## pnpm i
+
+## pnpm dev:v-vue3 or pnpm dev:v-vue3-ts
--- apps/v-vue3/src/main.js
+++ apps/v-vue3/src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import './style.css';
+import '@shared/style/index.css';
 import App from './App.vue';
 
 createApp(App).mount('#app');
